75ecb2d437714d274e2133cac0788f5e92ae7557
ahmad aby ayyasi <abiayyasi12@gmail.com>
2024-08-11 21:25:56 +0700

    fix: bug fix

--- src/constants/categories.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "@/types/category";
-
-export const categories: Category[] = [
-  { id: 1, name: "Bed", products: [] },
-  { id: 2, name: "Trolley", products: [] },
-  { id: 3, name: "Cabinet", products: [] },
-];
